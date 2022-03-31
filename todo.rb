5131b151b496b1e78aa9e973ee1bd0f383ddd712
Ethan Weiner <ethanweiner20@gmail.com>
2022-03-31 11:14:51 -0400

    Add todo name validation

--- todo.rb
+++ todo.rb
@@ -13,11 +13,16 @@ configure do
 end
 
 MESSAGES = {
+  # Success
   list_created: "The list has been created.",
   list_edited: "The list has been updated.",
   list_deleted: "The list has been deleted.",
+  todo_added: "The todo was added.",
+
+  # Error
   invalid_list_name: "List name must be between 1 and 100 characters.",
-  not_unique: "The list name must be unique."
+  not_unique: "The list name must be unique.",
+  invalid_todo_name: "Todo must be between 1 and 100 characters."
 }
 
 # FILTERS
@@ -28,27 +33,35 @@ before do
 end
 
 # Create a list hook for any routes involving a list
-before /\/lists\/\d+.*/ do
-  @list = session[:lists][params[:id].to_i]
+before %r(\/lists\/\d+.*) do
+  @list = session[:lists][params[:list_id].to_i]
 end
 
 # HELPERS
 
-# list_name_error: String -> Error?
+# list_name_error: String -> Maybe Symbol
 # Returns an error symbol if the list name is invalid, otherwise returns nil
 def list_name_error(name)
-  if !valid_size?(name)
+  if !valid_length?(name)
     :invalid_list_name
-  elsif !unique?(name)
+  elsif !unique_list_name?(name)
     :not_unique
   end
 end
 
-def valid_size?(name)
+# `todo_name_error`: String -> Maybe Symbol
+# Returns an error symbol if the todo name is invalid, ottherwise returns nil
+def todo_name_error(name)
+  if !valid_length?(name)
+    :invalid_todo_name
+  end
+end
+
+def valid_length?(name)
   name.length.between?(1, 100)
 end
 
-def unique?(name)
+def unique_list_name?(name)
   list_names = session[:lists].map { |list| list[:name] }
   !list_names.include?(name)
 end
@@ -75,7 +88,7 @@ get "/lists/new" do
 end
 
 # View singular list
-get "/lists/:id" do
+get "/lists/:list_id" do
   erb :list
 end
 
@@ -95,40 +108,49 @@ post "/lists" do
   end
 end
 
-
 # Render edit list form
-get "/lists/:id/edit" do
+get "/lists/:list_id/edit" do
   erb :edit_list
 end
 
 # Edit existing list
-post "/lists/:id" do  
+post "/lists/:list_id" do
   list_name = params["list-name"].strip
   error = list_name_error(list_name)
-  
+
   if error
     set_flash(error, :error)
     erb :edit_list
   else
     @list[:name] = list_name
     set_flash(:list_edited, :success)
-    redirect "/lists/#{params[:id]}"
+    redirect "/lists/#{params[:list_id]}"
   end
 end
 
 # Delete an existing list
-post "/lists/:id/destroy" do
-  session[:lists].delete_at(params[:id].to_i)
+post "/lists/:list_id/destroy" do
+  session[:lists].delete_at(params[:list_id].to_i)
   set_flash(:list_deleted, :success)
   redirect "/lists"
 end
 
 # Add a todo (to the current list)
-post "/lists/:id/todos" do
-  todo_name = params["todo-name"]
-  @list[:todos] << { name: todo_name, completed: false }
+post "/lists/:list_id/todos" do
+  todo_name = params["todo-name"].strip
 
-  redirect "/lists/#{params[:id]}"
+  error = todo_name_error(todo_name)
+  if error
+    set_flash(error, :error)
+
+    # Capture posted values for re-rendering
+    @previous_todo_name = params["todo-name"]
+    erb :list
+  else
+    @list[:todos] << { name: todo_name, completed: false }
+    set_flash(:todo_added, :success)
+    redirect "/lists/#{params[:list_id]}"
+  end
 end
 
 # Next: Extract list assignment to a before filter
