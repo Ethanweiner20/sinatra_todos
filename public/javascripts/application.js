3ded46fc0058f9d72447ded8fb5926c9d5d0da02
Ethan Weiner <ethanweiner20@gmail.com>
2022-04-02 10:37:39 -0400

    Add confirm dialog for deletion

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,14 @@
-// Require confirmation for all deletions
-$("button[type='submit'].delete").on("click", function (e) {
-  e.preventDefault();
-  console.log("hello");
-  return confirm("Are you sure?"); // Returns true only if confirmed
+$(function () {
+  // Require confirmation for all deletions
+
+  // $("form.delete").submit(function () {
+  //   return confirm("Are you sure?"); // Returns true only if confirmed
+  // });
+
+  $("form.delete").submit(function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (confirm("Are you sure?")) this.submit();
+  });
 });
